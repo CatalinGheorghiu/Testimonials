8a70f1e540cb9c5045a00df0f42aa58cd8be459a
Ionut Catalin GHEORGHIU <ionutcatalingheorghiu@pop-os.localdomain>
2020-05-30 23:49:28 +0200

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+    purge: [],
+    theme: {
+        fontFamily: {
+            body: ["Inter", "sans-serif"],
+        },
+        extend: {
+            maxHeight: {
+                "(screen-16)": "calc(100vh - 4rem)",
+            },
+            height: {
+                "card-size": "450px",
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
